fc39465e24f19f18426cacdeda12d62313ef8fe0
Trillium <spiteless@gmail.com>
2020-07-24 13:50:57 -0700

    Posts properly displaying text, date

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
+      api: "https://practiceapi.devmountain.com/api/",
       posts: []
     };
 
@@ -19,7 +22,15 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    let apiCall = this.state.api + "posts"
+    axios.get(apiCall).then( res => {
+        console.log('Successful pull from ' + apiCall)
+        console.log(res)
+        this.setState({
+          posts: res.data
+        })
+      })
+      .catch( err => { console.log(err)})
   }
 
   updatePost() {
@@ -37,6 +48,23 @@ class App extends Component {
   render() {
     const { posts } = this.state;
 
+    let displayPosts = posts.map( p => {
+      return ( 
+        <Post key= { p.id }
+              id= { p.id }
+              text= { p.text }
+              date = { p.date }
+              updatePost = { this.updatePost }
+              deletePost = { this.deletePost}
+          />
+        )
+      })
+      
+    
+    console.log("posts",posts)
+    console.log(displayPosts)
+    for (let p in posts) console.log(posts[p])
+
     return (
       <div className="App__parent">
         <Header />
@@ -44,7 +72,7 @@ class App extends Component {
         <section className="App__content">
 
           <Compose />
-          
+          {displayPosts}
         </section>
       </div>
     );
